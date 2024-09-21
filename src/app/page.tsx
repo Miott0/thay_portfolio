ccef320dd863add5da051252561b13979a3ec524
Rafael Miotte <miotterafael@.com>
2024-09-21 20:17:42 -0300

    correcoes de layout e otimizacoes na paginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import MaxWidhtWrapper from "@/components/MaxWidthWrapper";
 import { CardList } from "../components/card-list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-11  sm:p-24">
-      <CardList/>
-    </main>
+    <>
+      <MaxWidhtWrapper>
+        <CardList/>
+      </MaxWidhtWrapper>
+    </>
+    
   );
 }
